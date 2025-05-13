5347cce95dbc71f630f993e1cc7c927c1eda75c6
GOEUNNIM <qp0206@gmail.com>
2025-05-13 16:17:34 +0900

    구독 페이지 작업250513

--- js/subscription.js
+++ js/subscription.js
@@ -1,17 +1,3 @@
 $(function () {
-    // top → 왼쪽에서 오른쪽으로
-    $('.cont_top ul').simplyScroll({
-        direction: 'forwards', // ←→ 기본값: 왼→오
-        speed: 2,
-        pauseOnHover: false,
-        frameRate: 60
-    });
 
-    // bottom → 오른쪽에서 왼쪽으로
-    $('.con_bottom ul').simplyScroll({
-        direction: 'backwards', // →← 반대 방향
-        speed: 2,
-        pauseOnHover: false,
-        frameRate: 60
-    });
 })
